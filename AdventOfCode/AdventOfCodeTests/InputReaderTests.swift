b6e729048014b7ce4c221ef331e0c2bf0848e797
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-04-22 11:03:47 +0800

    - load file

--- AdventOfCode/AdventOfCodeTests/InputReaderTests.swift
+++ AdventOfCode/AdventOfCodeTests/InputReaderTests.swift
@@ -9,7 +9,12 @@ import XCTest
 
 class InputReader {
     
+    enum InputReaderError: Error {
+        case notFound
+    }
+    
     private let filePath: String
+    private var fileHandle: FileHandle?
     
     init?(in bundle: Bundle, file: String, ofType type: String) {
         guard let path = bundle.path(forResource: file, ofType: type) else {
@@ -18,8 +23,13 @@ class InputReader {
         self.filePath = path
     }
     
-    func load() {
+    func load() throws {
+        guard let fileHandle = FileHandle(forReadingAtPath: filePath) else {
+            throw InputReaderError.notFound
+        }
         
+        self.fileHandle = fileHandle
+        try fileHandle.seek(toOffset: 0)
     }
 }
 
@@ -33,6 +43,18 @@ class InputReaderTests: XCTestCase {
     func test_init_with_existingFile() {
         let sut = makeSUT()
         XCTAssertNotNil(sut, "Expected InputReader nil, got \(String(describing: sut)) instead")
+    }
+    
+    func test_load_throws_error_on_missing_file() {
+        let sut = makeSUT()
+        let path = Bundle(for: Self.self).path(forResource: "InputTest", ofType: "text")!
+        try! FileManager.default.removeItem(atPath: path)
+        do {
+            try sut?.load()
+            XCTFail("Expected error, got success instead")
+        } catch {
+            XCTAssertEqual(error as? InputReader.InputReaderError, InputReader.InputReaderError.notFound)
+        }
     }
     
     func makeSUT(_ file: String = "InputTest", _ type: String = "text") -> InputReader! {
